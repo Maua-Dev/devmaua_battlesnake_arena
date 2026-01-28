be894e151f0259721426e81f90709d04850fb602
Vitor SOLLER <vgsoller@gmail.com>
2026-01-28 18:16:57 -0300

    add customizations

--- server/src/components/page.rs
+++ server/src/components/page.rs
@@ -21,6 +21,7 @@ impl Render for Page {
         html! {
             head {
                 title { (self.title) }
+                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
                 link rel="stylesheet" href="/static/styles.css";
                 script src="/static/viewTransition.js" {}
             }
